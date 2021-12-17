ee8b057f87897f08c3d34c7ebff5cd5c94422185
MrTrukiny <carloseduardo.tkn@gmail.com>
2021-12-17 07:15:58 -0500

    Render SideDrawer as a Portal

--- src/shared/components/Navigation/SideDrawer.jsx
+++ src/shared/components/Navigation/SideDrawer.jsx
@@ -1,7 +1,11 @@
+import { createPortal } from 'react-dom';
+
 import './SideDrawer.css';
 
-const SideDrawer = props => {
-  return <aside className="side-drawer">{props.children}</aside>
+const SideDrawer = (props) => {
+  const content = <aside className="side-drawer">{props.children}</aside>;
+
+  return createPortal(content, document.getElementById('side-drawer'));
 };
 
 export default SideDrawer;
