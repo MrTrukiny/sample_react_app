4366f8ce4a5268961fa73d956c6c1b33fc67eeda
MrTrukiny <carloseduardo.tkn@gmail.com>
2021-12-16 20:40:03 -0500

    Create UsersList and UserItem components, and Users page with its styles

---
+++ src/user/pages/Users.jsx
@@ -0,0 +1,16 @@
+import UsersList from '../components/UsersList';
+
+const Users = () => {
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Eduardo Hernández',
+      image: 'https://avatars.githubusercontent.com/u/3603321?v=4',
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
